30fdeba3587793ba18eb13026da2913088baf8c8
Yogesh Vishwanath <yogeshmagatam@gmail.com>
2026-01-29 12:57:26 +0530

    Deployed in vercel and done changes

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  root: 'frontend',
+  build: {
+    outDir: '../dist'
+  },
+  server: {
+    port: 3000,
+    host: '0.0.0.0'
+  }
+})
